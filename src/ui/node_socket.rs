c9c2390aa936fe494ad6996d29e8bbbb797ebb8a
Paul Ogris <paul@tsahyt.com>
2020-02-14 10:30:01 +0100

    Node Socket basics

--- src/ui/node_socket.rs
+++ src/ui/node_socket.rs
@@ -1,3 +1,5 @@
+use super::subclass::*;
+use gdk::prelude::*;
 use glib::subclass;
 use glib::subclass::prelude::*;
 use glib::translate::*;
@@ -36,7 +38,27 @@ impl ObjectSubclass for NodeSocketPrivate {
     // type is created. Here class specific settings can be performed,
     // including installation of properties and registration of signals
     // for the new type.
-    // fn class_init(_class: &mut subclass::simple::ClassStruct<Self>) {}
+    fn class_init(class: &mut subclass::simple::ClassStruct<Self>) {
+        unsafe {
+            // Extra overrides for container methods
+            let container_class =
+                &mut *(class as *mut _ as *mut <gtk::Container as ObjectType>::RustClassType);
+
+            // Extra overrides for widget methods
+            let widget_class = &mut *(container_class as *mut _
+                as *mut <gtk::Widget as ObjectType>::RustClassType);
+            {
+                let klass =
+                    &mut *(widget_class as *mut gtk::WidgetClass as *mut gtk_sys::GtkWidgetClass);
+                klass.realize = Some(extra_widget_realize::<NodeSocketPrivate>);
+                klass.unrealize = Some(extra_widget_unrealize::<NodeSocketPrivate>);
+                klass.map = Some(extra_widget_map::<NodeSocketPrivate>);
+                klass.unmap = Some(extra_widget_unmap::<NodeSocketPrivate>);
+                klass.size_allocate = Some(extra_widget_size_allocate::<NodeSocketPrivate>);
+            }
+        }
+    }
+
 
     // Called every time a new instance is created. This should return
     // a new instance of our type with its basic values.
@@ -55,6 +77,81 @@ impl ObjectImpl for NodeSocketPrivate {
 }
 
 impl gtk::subclass::widget::WidgetImpl for NodeSocketPrivate {}
+
+impl WidgetImplExtra for NodeSocketPrivate {
+    fn map(&self, _widget: &gtk::Widget) {
+        // TODO: parent map
+
+        if let Some(ew) = self.event_window.borrow().as_ref() {
+            ew.show()
+        }
+    }
+
+    fn unmap(&self, _widget: &gtk::Widget) {
+        if let Some(ew) = self.event_window.borrow().as_ref() {
+            ew.show()
+        }
+
+        // TODO: parent unmap
+    }
+
+    fn realize(&self, widget: &gtk::Widget) {
+        widget.set_realized(true);
+        let parent_window = widget.get_parent_window().expect("Node Socket without parent window!");
+        let allocation = widget.get_allocation();
+
+        let mut event_mask = widget.get_events();
+        event_mask.insert(gdk::EventMask::BUTTON_PRESS_MASK);
+        event_mask.insert(gdk::EventMask::BUTTON_RELEASE_MASK);
+        event_mask.insert(gdk::EventMask::POINTER_MOTION_MASK);
+        event_mask.insert(gdk::EventMask::TOUCH_MASK);
+        event_mask.insert(gdk::EventMask::ENTER_NOTIFY_MASK);
+        event_mask.insert(gdk::EventMask::LEAVE_NOTIFY_MASK);
+
+        let window = gdk::Window::new(
+            Some(&parent_window),
+            &gdk::WindowAttr {
+                window_type: gdk::WindowType::Child,
+                x: Some(allocation.x),
+                y: Some(allocation.y),
+                width: (2.0 * self.radius) as _,
+                height: (2.0 * self.radius) as _,
+                wclass: gdk::WindowWindowClass::InputOnly,
+                event_mask: event_mask.bits() as _,
+                ..gdk::WindowAttr::default()
+            },
+        );
+
+        widget.register_window(&window);
+        self.event_window.replace(Some(window));
+    }
+
+    fn unrealize(&self, widget: &gtk::Widget) {
+        if let Some(ew) = self.event_window.borrow().as_ref() {
+            widget.unregister_window(ew);
+            ew.destroy();
+            self.event_window.replace(None);
+        }
+
+        // TODO: emit node socket destroyed signal
+        // TODO: unrealize parent
+    }
+
+    fn size_allocate(&self, widget: &gtk::Widget, allocation: &mut gtk::Allocation) {
+        widget.set_allocation(allocation);
+
+        if widget.get_realized() {
+            if let Some(ew) = self.event_window.borrow().as_ref() {
+                ew.move_resize(
+                    allocation.x,
+                    allocation.y,
+                    (2.0 * self.radius) as _,
+                    (2.0 * self.radius) as _,
+                );
+            }
+        }
+    }
+}
 
 glib_wrapper! {
     pub struct NodeSocket(
