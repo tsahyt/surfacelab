4cae07e7ac4d0f81b38814297eb82365e95ce17c
Paul Ogris <paul@tsahyt.com>
2020-02-19 14:43:01 +0100

    Base Socket on EventBox
    This saves the manual window handling

--- src/ui/node_socket.rs
+++ src/ui/node_socket.rs
@@ -15,7 +15,6 @@ pub enum NodeSocketIO {
 }
 
 pub struct NodeSocketPrivate {
-    event_window: RefCell<Option<gdk::Window>>,
     rgba: RefCell<(f64, f64, f64, f64)>,
     radius: RefCell<f64>,
     io: RefCell<NodeSocketIO>,
@@ -29,7 +28,7 @@ pub struct NodeSocketPrivate {
 impl ObjectSubclass for NodeSocketPrivate {
     const NAME: &'static str = "NodeSocketPrivate";
 
-    type ParentType = gtk::Widget;
+    type ParentType = gtk::EventBox;
     type Instance = subclass::simple::InstanceStruct<Self>;
     type Class = subclass::simple::ClassStruct<Self>;
 
@@ -39,34 +38,12 @@ impl ObjectSubclass for NodeSocketPrivate {
     // type is created. Here class specific settings can be performed,
     // including installation of properties and registration of signals
     // for the new type.
-    fn class_init(class: &mut subclass::simple::ClassStruct<Self>) {
-        unsafe {
-            // Extra overrides for container methods
-            let container_class =
-                &mut *(class as *mut _ as *mut <gtk::Container as ObjectType>::RustClassType);
-
-            // Extra overrides for widget methods
-            let widget_class = &mut *(container_class as *mut _
-                as *mut <gtk::Widget as ObjectType>::RustClassType);
-            {
-                let klass =
-                    &mut *(widget_class as *mut gtk::WidgetClass as *mut gtk_sys::GtkWidgetClass);
-                klass.realize = Some(extra_widget_realize::<NodeSocketPrivate>);
-                klass.unrealize = Some(extra_widget_unrealize::<NodeSocketPrivate>);
-                klass.map = Some(extra_widget_map::<NodeSocketPrivate>);
-                klass.unmap = Some(extra_widget_unmap::<NodeSocketPrivate>);
-                klass.size_allocate = Some(extra_widget_size_allocate::<NodeSocketPrivate>);
-                // klass.motion_notify_event =
-                //     Some(extra_widget_motion_notify_event::<NodeSocketPrivate>);
-            }
-        };
-    }
+    fn class_init(_class: &mut subclass::simple::ClassStruct<Self>) {}
 
     // Called every time a new instance is created. This should return
     // a new instance of our type with its basic values.
     fn new() -> Self {
         Self {
-            event_window: RefCell::new(None),
             rgba: RefCell::new((0.53, 0.71, 0.3, 1.)),
             radius: RefCell::new(8.0),
             io: RefCell::new(NodeSocketIO::Disable),
@@ -79,6 +56,12 @@ impl ObjectSubclass for NodeSocketPrivate {
         }
     }
 }
+
+impl gtk::subclass::container::ContainerImpl for NodeSocketPrivate {}
+
+impl gtk::subclass::bin::BinImpl for NodeSocketPrivate {}
+
+impl gtk::subclass::event_box::EventBoxImpl for NodeSocketPrivate {}
 
 impl ObjectImpl for NodeSocketPrivate {
     glib_object_impl!();
@@ -150,88 +133,7 @@ impl gtk::subclass::widget::WidgetImpl for NodeSocketPrivate {
     }
 }
 
-impl WidgetImplExtra for NodeSocketPrivate {
-    fn map(&self, widget: &gtk::Widget) {
-        if let Some(ew) = self.event_window.borrow().as_ref() {
-            ew.show();
-        }
-
-        self.parent_map(widget);
-    }
-
-    fn unmap(&self, widget: &gtk::Widget) {
-        if let Some(ew) = self.event_window.borrow().as_ref() {
-            ew.hide();
-        }
-
-        self.parent_unmap(widget);
-    }
-
-    fn realize(&self, widget: &gtk::Widget) {
-        widget.set_realized(true);
-        let allocation = widget.get_allocation();
-        let attributes = gdk::WindowAttr {
-            window_type: gdk::WindowType::Child,
-            x: Some(allocation.x),
-            y: Some(allocation.y),
-            width: allocation.width,
-            height: allocation.height,
-            wclass: gdk::WindowWindowClass::InputOnly,
-            event_mask: {
-                let mut em = widget.get_events();
-                em.insert(gdk::EventMask::BUTTON_PRESS_MASK);
-                em.insert(gdk::EventMask::BUTTON_RELEASE_MASK);
-                em.insert(gdk::EventMask::POINTER_MOTION_MASK);
-                em.insert(gdk::EventMask::ENTER_NOTIFY_MASK);
-                em.insert(gdk::EventMask::LEAVE_NOTIFY_MASK);
-                em.insert(gdk::EventMask::TOUCH_MASK);
-                em.bits() as _
-            },
-            ..gdk::WindowAttr::default()
-        };
-
-        let window = widget
-            .get_parent_window()
-            .expect("Node Socket without parent!");
-        widget.set_window(&window);
-        // TODO: g_object_ref(window)?
-
-        let event_window = gdk::Window::new(Some(&window), &attributes);
-        event_window.raise();
-        widget.register_window(&event_window);
-        self.event_window.replace(Some(event_window));
-    }
-
-    fn unrealize(&self, widget: &gtk::Widget) {
-        let mut delete_window = false;
-
-        if let Some(ew) = self.event_window.borrow().as_ref() {
-            widget.unregister_window(ew);
-            ew.destroy();
-            delete_window = true;
-        }
-
-        if delete_window {
-            self.event_window.replace(None);
-        }
-
-        self.parent_unrealize(widget);
-    }
-
-    fn size_allocate(&self, widget: &gtk::Widget, allocation: &mut gtk::Allocation) {
-        widget.set_allocation(allocation);
-        if let Some(ew) = self.event_window.borrow().as_ref() {
-            ew.move_resize(
-                allocation.x,
-                allocation.y,
-                allocation.width,
-                allocation.height,
-            );
-        }
-
-        self.parent_size_allocate(widget, allocation);
-    }
-}
+impl WidgetImplExtra for NodeSocketPrivate {}
 
 impl NodeSocketPrivate {
     fn set_rgba(&self, red: f64, green: f64, blue: f64, alpha: f64) {
@@ -283,7 +185,7 @@ glib_wrapper! {
         Object<subclass::simple::InstanceStruct<NodeSocketPrivate>,
         subclass::simple::ClassStruct<NodeSocketPrivate>,
         NodeSocketClass>)
-        @extends gtk::Widget;
+        @extends gtk::Widget, gtk::Container, gtk::Bin, gtk::EventBox;
 
     match fn {
         get_type => || NodeSocketPrivate::get_type().to_glib(),
