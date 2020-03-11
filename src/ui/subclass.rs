54f8210a1a1e85b2769d94d11e8f86f5140a907c
Paul Ogris <paul@tsahyt.com>
2020-03-11 10:30:42 +0100

    Remove extra subclassing methods moved to gtk-rs
    Still a few more to go

--- src/ui/subclass.rs
+++ src/ui/subclass.rs
@@ -11,18 +11,6 @@ pub trait WidgetImplExtra: WidgetImplExtraExt + 'static {
         self.parent_unmap(widget);
     }
 
-    fn realize(&self, widget: &gtk::Widget) {
-        self.parent_realize(widget);
-    }
-
-    fn unrealize(&self, widget: &gtk::Widget) {
-        self.parent_unrealize(widget);
-    }
-
-    fn size_allocate(&self, widget: &gtk::Widget, allocation: &mut gtk::Allocation) {
-        self.parent_size_allocate(widget, allocation);
-    }
-
     fn motion_notify_event(
         &self,
         widget: &gtk::Widget,
@@ -35,9 +23,6 @@ pub trait WidgetImplExtra: WidgetImplExtraExt + 'static {
 pub trait WidgetImplExtraExt {
     fn parent_map(&self, widget: &gtk::Widget);
     fn parent_unmap(&self, widget: &gtk::Widget);
-    fn parent_realize(&self, widget: &gtk::Widget);
-    fn parent_unrealize(&self, widget: &gtk::Widget);
-    fn parent_size_allocate(&self, widget: &gtk::Widget, allocation: &mut gtk::Allocation);
     fn parent_motion_notify_event(
         &self,
         widget: &gtk::Widget,
@@ -68,39 +53,6 @@ impl<T: WidgetImplExtra + ObjectImpl> WidgetImplExtraExt for T {
         }
     }
 
-    fn parent_realize(&self, widget: &gtk::Widget) {
-        unsafe {
-            let data = self.get_type_data();
-            let parent_class = data.as_ref().get_parent_class() as *mut gtk_sys::GtkWidgetClass;
-            let f = (*parent_class)
-                .realize
-                .expect("No parent class impl for \"realize\"");
-            f(widget.to_glib_none().0)
-        }
-    }
-
-    fn parent_unrealize(&self, widget: &gtk::Widget) {
-        unsafe {
-            let data = self.get_type_data();
-            let parent_class = data.as_ref().get_parent_class() as *mut gtk_sys::GtkWidgetClass;
-            let f = (*parent_class)
-                .unrealize
-                .expect("No parent class impl for \"unrealize\"");
-            f(widget.to_glib_none().0)
-        }
-    }
-
-    fn parent_size_allocate(&self, widget: &gtk::Widget, allocation: &mut gtk::Allocation) {
-        unsafe {
-            let data = self.get_type_data();
-            let parent_class = data.as_ref().get_parent_class() as *mut gtk_sys::GtkWidgetClass;
-            let f = (*parent_class)
-                .size_allocate
-                .expect("No parent class impl for \"size_allocate\"");
-            f(widget.to_glib_none().0, allocation.to_glib_none_mut().0)
-        }
-    }
-
     fn parent_motion_notify_event(
         &self,
         widget: &gtk::Widget,
@@ -118,26 +70,6 @@ impl<T: WidgetImplExtra + ObjectImpl> WidgetImplExtraExt for T {
             )))
         }
     }
-}
-
-pub unsafe extern "C" fn extra_widget_realize<T: ObjectSubclass>(ptr: *mut gtk_sys::GtkWidget)
-where
-    T: WidgetImplExtra,
-{
-    let instance = &*(ptr as *mut T::Instance);
-    let imp = instance.get_impl();
-    let wrap: gtk::Widget = from_glib_borrow(ptr);
-    imp.realize(&wrap);
-}
-
-pub unsafe extern "C" fn extra_widget_unrealize<T: ObjectSubclass>(ptr: *mut gtk_sys::GtkWidget)
-where
-    T: WidgetImplExtra,
-{
-    let instance = &*(ptr as *mut T::Instance);
-    let imp = instance.get_impl();
-    let wrap: gtk::Widget = from_glib_borrow(ptr);
-    imp.unrealize(&wrap);
 }
 
 pub unsafe extern "C" fn extra_widget_map<T: ObjectSubclass>(ptr: *mut gtk_sys::GtkWidget)
@@ -158,20 +90,6 @@ where
     let imp = instance.get_impl();
     let wrap: gtk::Widget = from_glib_borrow(ptr);
     imp.unmap(&wrap);
-}
-
-pub unsafe extern "C" fn extra_widget_size_allocate<T: ObjectSubclass>(
-    ptr: *mut gtk_sys::GtkWidget,
-    aptr: *mut gtk_sys::GtkAllocation,
-) where
-    T: WidgetImplExtra,
-{
-    let instance = &*(ptr as *mut T::Instance);
-    let imp = instance.get_impl();
-    let wrap: gtk::Widget = from_glib_borrow(ptr);
-    let mut alloc: gtk::Allocation = from_glib_borrow(aptr);
-
-    imp.size_allocate(&wrap, &mut alloc);
 }
 
 pub unsafe extern "C" fn extra_widget_motion_notify_event<T: ObjectSubclass>(
