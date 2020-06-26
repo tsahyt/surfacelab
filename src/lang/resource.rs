22e80ae10e9f31e138b50d032660697c45b20312
Paul Ogris <paul@tsahyt.com>
2020-06-26 12:02:47 +0200

    Strong typing for resource schemas

--- src/lang/resource.rs
+++ src/lang/resource.rs
@@ -1,11 +1,39 @@
 use serde_derive::{Deserialize, Serialize};
 use std::path::{Path, PathBuf};
+use std::convert::TryFrom;
 
 pub type ResourcePart = String;
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub enum Scheme {
+    Node,
+    Graph
+}
+
+impl std::fmt::Display for Scheme {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Node => write!(f, "node"),
+            Self::Graph => write!(f, "graph"),
+        }
+    }
+}
+
+impl TryFrom<&str> for Scheme {
+    type Error = &'static str;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match value {
+            "node" => Ok(Self::Node),
+            "graph" => Ok(Self::Graph),
+            _ => Err("Unknown Scheme")
+        }
+    }
+}
+
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Resource {
-    scheme: String,
+    scheme: Scheme,
     resource_path: PathBuf,
     fragment: Option<String>,
 }
@@ -31,16 +59,15 @@ impl std::fmt::Display for Resource {
     }
 }
 
-impl std::convert::TryFrom<&str> for Resource {
+impl TryFrom<&str> for Resource {
     type Error = &'static str;
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
         let pieces: Vec<&str> = value.split(':').collect();
 
-        let scheme = (*pieces
+        let scheme = Scheme::try_from(*pieces
             .get(0)
-            .ok_or("Missing schema in resource identifier")?)
-        .to_string();
+            .ok_or("Missing schema in resource identifier")?)?;
         let resource_path =
             PathBuf::from(pieces.get(1).ok_or("Missing path in resource identifier")?);
         let fragment = pieces.get(2).map(|x| (*x).to_string());
@@ -54,6 +81,9 @@ impl std::convert::TryFrom<&str> for Resource {
 }
 
 impl Resource {
+    // pub fn new<P: AsRef<Path>>(scheme: Scheme, path: P, fragment: Option<String>) -> Self {
+    // }
+
     pub fn fragment(&self) -> Option<&str> {
         self.fragment.as_ref().map(|x| x.as_ref())
     }
@@ -70,8 +100,8 @@ impl Resource {
         new
     }
 
-    pub fn scheme(&self) -> &str {
-        &self.scheme
+    pub fn scheme(&self) -> Scheme {
+        self.scheme
     }
 
     pub fn path(&self) -> &Path {
@@ -80,7 +110,7 @@ impl Resource {
 
     pub fn unregistered_node() -> Resource {
         Resource {
-            scheme: "node".to_string(),
+            scheme: Scheme::Node,
             resource_path: PathBuf::from("__unregistered__"),
             fragment: None,
         }
@@ -106,7 +136,7 @@ mod tests {
     fn test_resource_parse_node() {
         let x = Resource::try_from("node:/foo/bar-01").unwrap();
         assert_eq!(x.fragment, None);
-        assert_eq!(x.scheme, "node");
+        assert_eq!(x.scheme, Scheme::Node);
         assert_eq!(x.resource_path, PathBuf::from("/foo/bar-01"));
     }
 
@@ -115,20 +145,20 @@ mod tests {
         // simple
         let x = Resource::try_from("node:/foo:socket_in").unwrap();
         assert_eq!(x.fragment, Some("socket_in".to_string()));
-        assert_eq!(x.scheme, "node");
+        assert_eq!(x.scheme, Scheme::Node);
         assert_eq!(x.resource_path, PathBuf::from("/foo"));
 
         // in nested node
         let x = Resource::try_from("node:/foo/bar-01:socket").unwrap();
         assert_eq!(x.fragment, Some("socket".to_string()));
-        assert_eq!(x.scheme, "node");
+        assert_eq!(x.scheme, Scheme::Node);
         assert_eq!(x.resource_path, PathBuf::from("/foo/bar-01"));
     }
 
     #[test]
     fn test_resource_display() {
         let r = Resource {
-            scheme: "node".to_string(),
+            scheme: Scheme::Node,
             resource_path: PathBuf::from("/foo/bar"),
             fragment: Some("socket".to_string()),
         };
