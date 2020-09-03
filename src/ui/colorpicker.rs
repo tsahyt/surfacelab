212b925e57b2d367a0c90f1855572878882f4d20
Paul Ogris <paul@tsahyt.com>
2020-09-03 18:43:57 +0200

    Saturation/Value rectangle drawing

--- src/ui/colorpicker.rs
+++ src/ui/colorpicker.rs
@@ -49,10 +49,15 @@ impl Widget for ColorPicker<Hsv> {
     }
 
     fn update(self, args: widget::UpdateArgs<Self>) -> Self::Event {
-        let tris = color_strip(4, 256.0, 24.0, |x| {
+        let bar_tris = color_strip(6, 256.0, 24.0, |x| {
             color::hsl(x as f32 * std::f32::consts::TAU, 1.0, 0.5).to_rgb()
         });
-        widget::Triangles::multi_color(tris)
+        let rect_tris = color_rect(4, 256.0, 256.0, |x,y| {
+            let hsv = palette::Hsv::new::<f32>(150.0, x as f32, y as f32);
+            let rgb = palette::LinSrgb::from(hsv);
+            color::Rgba(rgb.red, rgb.green, rgb.blue, 1.0)
+        });
+        widget::Triangles::multi_color(rect_tris)
             .with_bounding_rect(args.rect)
             .parent(args.id)
             .middle()
@@ -71,7 +76,7 @@ fn color_strip<F: Fn(f64) -> color::Rgba>(
     height: f64,
     color: F,
 ) -> Vec<Triangle<ColoredPoint>> {
-    let mut tris = Vec::new();
+    let mut tris = Vec::with_capacity((2 as usize).pow(k as u32 + 1));
     let step = 1.0 / (k as f64).exp2();
     let mut x: f64 = 0.0;
 
@@ -79,7 +84,7 @@ fn color_strip<F: Fn(f64) -> color::Rgba>(
     let bottom = -height / 2.0;
     let left = -width / 2.0;
 
-    for _ in 1..(2 as u16).pow(k as _) {
+    for _ in 0..(2 as u16).pow(k as _) {
         // Current color
         let c = color(x);
 
@@ -118,6 +123,41 @@ fn color_rect<F: Fn(f64, f64) -> color::Rgba>(
     height: f64,
     color: F,
 ) -> Vec<Triangle<ColoredPoint>> {
-    // TODO geometry construction with Z order curve
-    todo!()
+    let mut tris = Vec::with_capacity((4 as usize).pow(k as _) * 2);
+    let step = 1.0 / (k as f64).exp2();
+    let mut x: f64;
+    let mut y: f64 = 0.0;
+
+    let bottom = -height / 2.0;
+    let left = -width / 2.0;
+
+    for _ in 0..(2 as u16).pow(k as _) {
+        x = 0.0;
+        for _ in 0..(2 as u16).pow(k as _) {
+            let c_bl = color(x, y);
+            let c_br = color(x + step, y);
+            let c_tl = color(x, y + step);
+            let c_tr = color(x + step, y + step);
+
+            let l = left + x * width;
+            let r = left + (x + step) * width;
+            let b = bottom + y * height;
+            let t = bottom + (y + step) * height;
+
+            tris.push(Triangle([
+                ([l,b], c_bl),
+                ([l,t], c_tl),
+                ([r,t], c_tr),
+            ]));
+            tris.push(Triangle([
+                ([l,b], c_bl),
+                ([r,t], c_tr),
+                ([r,b], c_br),
+            ]));
+            x += step;
+        }
+        y += step;
+    }
+
+    tris
 }
