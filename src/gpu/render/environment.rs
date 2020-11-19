a1794e6766774a437ab2304e0529fc2b701f1861
Paul Ogris <paul@tsahyt.com>
2020-11-19 17:27:32 +0100

    Environment map compute pipeline stubs

--- src/gpu/render/environment.rs
+++ src/gpu/render/environment.rs
@@ -6,6 +6,8 @@ use std::mem::ManuallyDrop;
 use std::path::Path;
 use std::sync::{Arc, Mutex};
 
+static IRRADIANCE_SHADER: &[u8] = include_bytes!("../../../shaders/irradiance.spv");
+
 pub struct EnvironmentMaps<B: Backend> {
     gpu: Arc<Mutex<GPU<B>>>,
     irradiance_image: ManuallyDrop<B::Image>,
@@ -34,10 +36,12 @@ where
                 hal::image::Usage::SAMPLED | hal::image::Usage::STORAGE,
                 hal::image::ViewCapabilities::KIND_CUBE,
             )
-        }.map_err(|_| "Failed to acquire cube map image")?;
+        }
+        .map_err(|_| "Failed to acquire cube map image")?;
 
         let requirements = unsafe { lock.device.get_image_requirements(&irradiance_image) };
-        let memory_type = lock.memory_properties
+        let memory_type = lock
+            .memory_properties
             .memory_types
             .iter()
             .position(|mem_type| {
@@ -47,9 +51,14 @@ where
             })
             .unwrap()
             .into();
-        let irradiance_memory = unsafe { lock.device.allocate_memory(memory_type, requirements.size) }
-            .map_err(|_| "Failed to allocate memory for cube map")?;
-        unsafe { lock.device.bind_image_memory(&irradiance_memory, 0, &mut irradiance_image) }.unwrap();
+        let irradiance_memory =
+            unsafe { lock.device.allocate_memory(memory_type, requirements.size) }
+                .map_err(|_| "Failed to allocate memory for cube map")?;
+        unsafe {
+            lock.device
+                .bind_image_memory(&irradiance_memory, 0, &mut irradiance_image)
+        }
+        .unwrap();
 
         let irradiance_view = unsafe {
             lock.device.create_image_view(
@@ -86,22 +95,113 @@ where
         let env_maps = Self::init(gpu, cubemap_size)?;
 
         // Read data from file
-        let reader =
-            BufReader::new(File::open(path).map_err(|_| "Failed to open HDRi file")?);
-        let decoder =
-            hdr::HdrDecoder::new(reader).map_err(|_| "Failed to decode HDRi file")?;
+        let reader = BufReader::new(File::open(path).map_err(|_| "Failed to open HDRi file")?);
+        let decoder = hdr::HdrDecoder::new(reader).map_err(|_| "Failed to decode HDRi file")?;
         let metadata = decoder.metadata();
         let raw_hdri = decoder
             .read_image_hdr()
             .map_err(|_| "Failed to read from HDRi file")?;
 
         // Prepare compute pipeline
+        let lock = env_maps.gpu.lock().unwrap();
+
+        let command_pool = unsafe {
+            lock.device.create_command_pool(
+                lock.queue_group.family,
+                hal::pool::CommandPoolCreateFlags::empty(),
+            )
+        }
+        .map_err(|_| "")?;
+        let descriptor_pool = unsafe {
+            use hal::pso::*;
+            lock.device
+                .create_descriptor_pool(4, &[], DescriptorPoolCreateFlags::empty())
+        }
+        .map_err(|_| "Failed to create descriptor pool")?;
+
+        let set_layout = unsafe {
+            lock.device.create_descriptor_set_layout(
+                &[
+                    hal::pso::DescriptorSetLayoutBinding {
+                        binding: 0,
+                        ty: hal::pso::DescriptorType::Image {
+                            ty: hal::pso::ImageDescriptorType::Sampled {
+                                with_sampler: false,
+                            },
+                        },
+                        count: 1,
+                        stage_flags: hal::pso::ShaderStageFlags::COMPUTE,
+                        immutable_samplers: false,
+                    },
+                    hal::pso::DescriptorSetLayoutBinding {
+                        binding: 1,
+                        ty: hal::pso::DescriptorType::Sampler,
+                        count: 1,
+                        stage_flags: hal::pso::ShaderStageFlags::COMPUTE,
+                        immutable_samplers: false,
+                    },
+                    hal::pso::DescriptorSetLayoutBinding {
+                        binding: 2,
+                        ty: hal::pso::DescriptorType::Image {
+                            ty: hal::pso::ImageDescriptorType::Storage { read_only: false },
+                        },
+                        count: 1,
+                        stage_flags: hal::pso::ShaderStageFlags::COMPUTE,
+                        immutable_samplers: false,
+                    },
+                ],
+                &[],
+            )
+        }
+        .map_err(|_| "Failed to create descriptor set layout")?;
+
+        let pipeline_layout = unsafe { lock.device.create_pipeline_layout(Some(&set_layout), &[]) }
+            .map_err(|_| "Failed to create compute pipeline")?;
+
+        let irradiance_module = {
+            let loaded_spirv =
+                hal::pso::read_spirv(std::io::Cursor::new(IRRADIANCE_SHADER)).map_err(|_| "")?;
+            unsafe { lock.device.create_shader_module(&loaded_spirv) }.map_err(|_| "")?
+        };
+
+        let entry_point = hal::pso::EntryPoint {
+            entry: "main",
+            module: &irradiance_module,
+            specialization: hal::pso::Specialization::default(),
+        };
+
+        let pipeline = unsafe {
+            lock.device.create_compute_pipeline(
+                &hal::pso::ComputePipelineDesc::new(entry_point, &pipeline_layout),
+                None,
+            )
+        }
+        .map_err(|_| "Failed to create compute pipeline")?;
+
+        let fence = lock.device.create_fence(false).unwrap();
+        let sampler = unsafe {
+            lock.device.create_sampler(&hal::image::SamplerDesc::new(
+                hal::image::Filter::Linear,
+                hal::image::WrapMode::Tile,
+            ))
+        }
+        .map_err(|_| "Failed to create sampler")?;
 
         // Convolve irradiance map
 
         // TODO: Pre-filter environment map
 
         // Clean up compute pipeline
+        unsafe {
+            lock.device.destroy_command_pool(command_pool);
+            lock.device.destroy_sampler(sampler);
+            lock.device.destroy_descriptor_pool(descriptor_pool);
+            lock.device.destroy_descriptor_set_layout(set_layout);
+            lock.device.destroy_shader_module(irradiance_module);
+            lock.device.destroy_fence(fence);
+            lock.device.destroy_pipeline_layout(pipeline_layout);
+            lock.device.destroy_compute_pipeline(pipeline);
+        }
         todo!()
     }
 }
