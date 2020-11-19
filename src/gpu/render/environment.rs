bf29ad5fa00d81ffdad70f1d7e2fb4b27ed94e31
Paul Ogris <paul@tsahyt.com>
2020-11-19 12:18:04 +0100

    Environment maps image initialization

--- src/gpu/render/environment.rs
+++ src/gpu/render/environment.rs
@@ -17,10 +17,59 @@ impl<B> EnvironmentMaps<B>
 where
     B: Backend,
 {
+    const FORMAT: hal::format::Format = hal::format::Format::Rgba32Sfloat;
+
     /// Initialize GPU side structures for environment map without data, given a
     /// cubemap size.
-    fn init(gpu: Arc<Mutex<GPU<B>>>, size: usize) -> Result<Self, String> {
-        todo!()
+    fn init(gpu: Arc<Mutex<GPU<B>>>, size: usize) -> Result<Self, &'static str> {
+        let lock = gpu.lock().unwrap();
+
+        // Irradiance cube map
+        let mut irradiance_image = unsafe {
+            lock.device.create_image(
+                hal::image::Kind::D2(size as u32, size as u32, 6, 1),
+                1,
+                Self::FORMAT,
+                hal::image::Tiling::Linear,
+                hal::image::Usage::SAMPLED | hal::image::Usage::STORAGE,
+                hal::image::ViewCapabilities::KIND_CUBE,
+            )
+        }.map_err(|_| "Failed to acquire cube map image")?;
+
+        let requirements = unsafe { lock.device.get_image_requirements(&irradiance_image) };
+        let memory_type = lock.memory_properties
+            .memory_types
+            .iter()
+            .position(|mem_type| {
+                mem_type
+                    .properties
+                    .contains(hal::memory::Properties::DEVICE_LOCAL)
+            })
+            .unwrap()
+            .into();
+        let irradiance_memory = unsafe { lock.device.allocate_memory(memory_type, requirements.size) }
+            .map_err(|_| "Failed to allocate memory for cube map")?;
+        unsafe { lock.device.bind_image_memory(&irradiance_memory, 0, &mut irradiance_image) }.unwrap();
+
+        let irradiance_view = unsafe {
+            lock.device.create_image_view(
+                &irradiance_image,
+                hal::image::ViewKind::Cube,
+                Self::FORMAT,
+                hal::format::Swizzle::NO,
+                super::super::COLOR_RANGE.clone(),
+            )
+        }
+        .map_err(|_| "Failed to create cube map view")?;
+
+        drop(lock);
+
+        Ok(Self {
+            gpu,
+            irradiance_image: ManuallyDrop::new(irradiance_image),
+            irradiance_memory: ManuallyDrop::new(irradiance_memory),
+            irradiance_view: ManuallyDrop::new(irradiance_view),
+        })
     }
 
     /// Create environment maps from a path to a HDRi file. Expects .hdr,
@@ -29,7 +78,7 @@ where
         gpu: Arc<Mutex<GPU<B>>>,
         cubemap_size: usize,
         path: P,
-    ) -> Result<Self, String> {
+    ) -> Result<Self, &'static str> {
         use std::fs::File;
         use std::io::BufReader;
 
@@ -38,19 +87,19 @@ where
 
         // Read data from file
         let reader =
-            BufReader::new(File::open(path).map_err(|_| "Failed to open HDRi file".to_string())?);
+            BufReader::new(File::open(path).map_err(|_| "Failed to open HDRi file")?);
         let decoder =
-            hdr::HdrDecoder::new(reader).map_err(|_| "Failed to decode HDRi file".to_string())?;
+            hdr::HdrDecoder::new(reader).map_err(|_| "Failed to decode HDRi file")?;
         let metadata = decoder.metadata();
         let raw_hdri = decoder
             .read_image_hdr()
-            .map_err(|_| "Failed to read from HDRi file".to_string())?;
+            .map_err(|_| "Failed to read from HDRi file")?;
 
         // Prepare compute pipeline
 
         // Convolve irradiance map
 
-        // Pre-filter environment map
+        // TODO: Pre-filter environment map
 
         // Clean up compute pipeline
         todo!()
