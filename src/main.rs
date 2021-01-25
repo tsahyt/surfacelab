9f2894d17cbebc2ad683763faa0281639affbdd4
Paul Ogris <paul@tsahyt.com>
2021-01-25 19:19:21 +0100

    Remove redundant clone in main

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ fn main() {
 
     // initialize GPU before proceeding
     match gpu::initialize_gpu(true) {
-        #[allow(clippy::redundant_clone)]
         Ok(gpu) => {
             // initialize the bus
             let mut broker: broker::Broker<lang::Lang> = broker::Broker::new(1024);
@@ -16,7 +15,7 @@ fn main() {
             let nodes_thread = surfacelab::nodes::start_nodes_thread(&mut broker);
             let compute_thread =
                 surfacelab::compute::start_compute_thread(&mut broker, gpu.clone());
-            let render_thread = surfacelab::render::start_render_thread(&mut broker, gpu.clone());
+            let render_thread = surfacelab::render::start_render_thread(&mut broker, gpu);
             let _broker_runner = thread::spawn(move || broker.run());
 
             // wait for threads
