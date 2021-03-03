5ca1a8a2c32e119249653c1ada23cd125ef284b7
Paul Ogris <paul@tsahyt.com>
2021-03-03 14:16:43 +0100

    Add extra options to Distance
    Not quite final perhaps, extent might be hard to use since it's
    exponential. Linear distance amounts however are hard to control in the
    lower ranges and that's where it intuitively seems to matter the most.
    Perhaps having controls that allow changing from exp to linear scale etc
    would be the best solution here.

--- src/lang/operators/distance.rs
+++ src/lang/operators/distance.rs
@@ -6,18 +6,50 @@ use crate::shader;
 use maplit::hashmap;
 use serde_derive::{Deserialize, Serialize};
 use std::collections::HashMap;
+use strum::VariantNames;
+use strum_macros::*;
 use surfacelab_derive::*;
 use zerocopy::AsBytes;
 
 #[repr(C)]
+#[derive(
+    AsBytes,
+    Clone,
+    Copy,
+    Debug,
+    EnumIter,
+    EnumVariantNames,
+    EnumString,
+    Serialize,
+    Deserialize,
+    ParameterField,
+    PartialEq,
+)]
+pub enum DistanceMetric {
+    Euclidean = 0,
+    Manhattan = 1,
+    Chebyshev = 2,
+}
+
+#[repr(C)]
 #[derive(AsBytes, Clone, Copy, Debug, Serialize, Deserialize, Parameters, PartialEq)]
 pub struct Distance {
+    metric: DistanceMetric,
+    clamp: ParameterBool,
+    expand: ParameterBool,
     threshold: f32,
+    extent: f32,
 }
 
 impl Default for Distance {
     fn default() -> Self {
-        Self { threshold: 0.5 }
+        Self {
+            metric: DistanceMetric::Euclidean,
+            clamp: 0,
+            expand: 0,
+            threshold: 0.5,
+            extent: 32.0,
+        }
     }
 }
 
@@ -124,12 +156,50 @@ impl OperatorParamBox for Distance {
                 name: "basic-parameters",
                 parameters: vec![
                     Parameter {
+                        name: "metric".to_string(),
+                        transmitter: Field(Distance::METRIC.to_string()),
+                        control: Control::Enum {
+                            selected: self.metric as usize,
+                            variants: DistanceMetric::VARIANTS
+                                .iter()
+                                .map(|x| x.to_string())
+                                .collect(),
+                        },
+                        expose_status: Some(ExposeStatus::Unexposed),
+                    },
+                    Parameter {
+                        name: "clamp".to_string(),
+                        transmitter: Field(Distance::CLAMP.to_string()),
+                        control: Control::Toggle {
+                            def: self.clamp == 1,
+                        },
+                        expose_status: Some(ExposeStatus::Unexposed),
+                    },
+                    Parameter {
+                        name: "expand".to_string(),
+                        transmitter: Field(Distance::EXPAND.to_string()),
+                        control: Control::Toggle {
+                            def: self.expand == 1,
+                        },
+                        expose_status: Some(ExposeStatus::Unexposed),
+                    },
+                    Parameter {
                         name: "threshold".to_string(),
                         transmitter: Field(Distance::THRESHOLD.to_string()),
                         control: Control::Slider {
                             value: self.threshold,
                             min: 0.,
                             max: 1.,
+                        },
+                        expose_status: Some(ExposeStatus::Unexposed),
+                    },
+                    Parameter {
+                        name: "extent".to_string(),
+                        transmitter: Field(Distance::EXTENT.to_string()),
+                        control: Control::Slider {
+                            value: self.extent,
+                            min: 0.,
+                            max: 10.,
                         },
                         expose_status: Some(ExposeStatus::Unexposed),
                     },
