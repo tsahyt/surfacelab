719f19c311736e216ca622faae0829822ef32cc7
Paul Ogris <paul@tsahyt.com>
2021-04-08 16:03:12 +0200

    Fix color picker positioning in ramp

--- src/ui/widgets/color_ramp.rs
+++ src/ui/widgets/color_ramp.rs
@@ -62,10 +62,12 @@ impl<'a> Widget for ColorRamp<'a> {
     }
 
     fn update(self, args: widget::UpdateArgs<Self>) -> Self::Event {
-        let xy = args.ui.xy_of(args.id).unwrap();
-        let wh = args.ui.wh_of(args.id).unwrap();
+        let widget::UpdateArgs { id, ui, state, rect, .. } = args;
 
-        let selected_step = self.ramp[args.state.selected];
+        let xy = rect.xy();
+        let wh = rect.w_h();
+
+        let selected_step = self.ramp[state.selected];
         let selected_position = selected_step[3];
         let selected_color = palette::Hsv::from(palette::LinSrgb::new(
             selected_step[0],
@@ -76,26 +78,26 @@ impl<'a> Widget for ColorRamp<'a> {
         let mut display_ramp: SmallVec<[_; 16]> = self.ramp.iter().copied().collect();
         display_ramp.sort_by(|a, b| a[3].partial_cmp(&b[3]).unwrap());
 
-        let gradient_tris = gradient_strip(display_ramp.as_slice(), wh[0], 24.0);
-        let gradient_pos = [xy[0], xy[1] + wh[1] / 2.0 - 12.0];
+        let gradient_tris = gradient_strip(display_ramp.as_slice(), wh.0, 24.0);
+        let gradient_pos = [xy[0], xy[1] + wh.1 / 2.0 - 12.0];
 
         widget::Triangles::multi_color(gradient_tris.iter().map(|t| t.add(gradient_pos)))
-            .with_bounding_rect(args.rect)
-            .set(args.state.ids.triangles, args.ui);
+            .with_bounding_rect(rect)
+            .set(state.ids.triangles, ui);
 
         let mut event = None;
 
-        let button_width = (wh[0] - (8.0 * 4.0)) / 5.0;
+        let button_width = (wh.0 - (8.0 * 4.0)) / 5.0;
         for _press in widget::Button::new()
             .label("Add")
-            .parent(args.id)
+            .parent(id)
             .label_font_size(10)
             .top_left_with_margins(32.0, 0.0)
             .w(button_width)
             .h(16.0)
-            .set(args.state.ids.add_step, args.ui)
+            .set(state.ids.add_step, ui)
         {
-            args.state.update(|state| {
+            state.update(|state| {
                 if state.selected > 0 {
                     state.selected += 1
                 }
@@ -105,52 +107,52 @@ impl<'a> Widget for ColorRamp<'a> {
 
         for _press in widget::Button::new()
             .label("Delete")
-            .parent(args.id)
+            .parent(id)
             .label_font_size(10)
             .w(button_width)
             .h(16.0)
             .right(8.0)
-            .set(args.state.ids.delete_step, args.ui)
+            .set(state.ids.delete_step, ui)
         {
-            event = Some(Event::DeleteStep(args.state.selected));
+            event = Some(Event::DeleteStep(state.selected));
         }
 
         for _press in widget::Button::new()
             .label("Next")
-            .parent(args.id)
+            .parent(id)
             .label_font_size(10)
             .w(button_width)
             .h(16.0)
             .right(8.0)
-            .set(args.state.ids.next_step, args.ui)
+            .set(state.ids.next_step, ui)
         {
-            args.state
+            state
                 .update(|state| state.selected = (state.selected + 1).min(self.ramp.len() - 1))
         }
 
         for _press in widget::Button::new()
             .label("Prev")
-            .parent(args.id)
+            .parent(id)
             .label_font_size(10)
             .w(button_width)
             .h(16.0)
             .right(8.0)
-            .set(args.state.ids.prev_step, args.ui)
+            .set(state.ids.prev_step, ui)
         {
-            args.state
+            state
                 .update(|state| state.selected = state.selected.saturating_sub(1))
         }
 
         if let Some(new_pos) = widget::NumberDialer::new(selected_position, 0.0, 1.0, 4)
-            .parent(args.id)
+            .parent(id)
             .label_font_size(10)
             .right(8.0)
             .w(button_width)
             .h(16.0)
-            .set(args.state.ids.step_dialer, args.ui)
+            .set(state.ids.step_dialer, ui)
         {
             event = Some(Event::ChangeStep(
-                args.state.selected,
+                state.selected,
                 [
                     selected_step[0],
                     selected_step[1],
@@ -161,14 +163,14 @@ impl<'a> Widget for ColorRamp<'a> {
         }
 
         if let Some(new_color) = ColorPicker::new(selected_color)
-            .wh([wh[0], wh[1] - 40.0])
-            .parent(args.id)
+            .wh([wh.0, wh.1 - 56.0])
+            .parent(id)
             .mid_bottom()
-            .set(args.state.ids.colorpicker, args.ui)
+            .set(state.ids.colorpicker, ui)
         {
             let rgb = palette::LinSrgb::from(new_color);
             event = Some(Event::ChangeStep(
-                args.state.selected,
+                state.selected,
                 [rgb.red, rgb.green, rgb.blue, selected_position],
             ));
         }
@@ -189,10 +191,10 @@ fn gradient_strip(steps: &[[f32; 4]], width: f64, height: f64) -> Vec<Triangle<C
     let top = height / 2.0;
 
     let mut x: f64 = left;
-    let mut color = color::Rgba(steps[0][0], steps[0][1], steps[0][2], 1.0);
+    let mut color = color::Rgba(steps[0][0], steps[0][1], steps[0][2], 0.5);
 
     for step in steps {
-        let next_color = color::Rgba(step[0], step[1], step[2], 1.0);
+        let next_color = color::Rgba(step[0], step[1], step[2], 0.5);
         let next_x = left + step[3] as f64 * width;
 
         tris.push(Triangle([
