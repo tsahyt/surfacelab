cfac5fe69a9aaadc3532c98ac3f113f24b2a9e88
Paul Ogris <paul@tsahyt.com>
2021-04-27 12:39:58 +0200

    Refactor bezier widget
    Adds a distance function as well

--- src/ui/widgets/bezier.rs
+++ src/ui/widgets/bezier.rs
@@ -81,21 +81,15 @@ impl Widget for Bezier {
         } = args;
 
         let num_points = 2_u32.pow(self.resolution as u32);
-        let bezier_points = (0..num_points + 1).map(|r| {
-            let t = r as f64 / num_points as f64;
-            let x = (1. - t).powi(3) * self.start[0]
-                + 3. * (1. - t).powi(2) * t * self.control_1[0]
-                + 3. * (1. - t) * t.powi(2) * self.control_2[0]
-                + t.powi(3) * self.end[0];
-            let y = (1. - t).powi(3) * self.start[1]
-                + 3. * (1. - t).powi(2) * t * self.control_1[1]
-                + 3. * (1. - t) * t.powi(2) * self.control_2[1]
-                + t.powi(3) * self.end[1];
-            [x, y]
-        });
 
         widget::PointPath::abs_styled(
-            bezier_points,
+            bezier_points(
+                num_points,
+                self.start,
+                self.control_1,
+                self.control_2,
+                self.end,
+            ),
             widget::primitive::line::Style {
                 maybe_pattern: Some(style.pattern(&ui.theme)),
                 maybe_color: Some(style.color(&ui.theme)),
@@ -108,4 +102,58 @@ impl Widget for Bezier {
         .graphics_for(id)
         .set(state.ids.point_path, ui);
     }
+}
+
+/// Produce points for the given bezier curve
+pub fn bezier_points(
+    num_points: u32,
+    start: Point,
+    control_1: Point,
+    control_2: Point,
+    end: Point,
+) -> impl Iterator<Item = Point> + Clone {
+    (0..num_points + 1).map(move |r| {
+        let t = r as f64 / num_points as f64;
+        let x = (1. - t).powi(3) * start[0]
+            + 3. * (1. - t).powi(2) * t * control_1[0]
+            + 3. * (1. - t) * t.powi(2) * control_2[0]
+            + t.powi(3) * end[0];
+        let y = (1. - t).powi(3) * start[1]
+            + 3. * (1. - t).powi(2) * t * control_1[1]
+            + 3. * (1. - t) * t.powi(2) * control_2[1]
+            + t.powi(3) * end[1];
+        [x, y]
+    })
+}
+
+/// Approximate distance to the bezier curve.
+pub fn approx_bezier_distance(
+    p: Point,
+    start: Point,
+    control_1: Point,
+    control_2: Point,
+    end: Point,
+) -> f64 {
+    use itertools::Itertools;
+    bezier_points(16, start, control_1, control_2, end)
+        .tuple_windows()
+        .map(|(a, b)| sd_segment(p, a, b))
+        .min_by(|a, b| a.partial_cmp(b).expect("Tried to compare a NaN"))
+        .unwrap_or(f64::MAX)
+}
+
+/// Signed distance to a line segment
+fn sd_segment(p: Point, a: Point, b: Point) -> f64 {
+    use nalgebra::*;
+
+    let p = Point2::from_slice(&p);
+    let a = Point2::from_slice(&a);
+    let b = Point2::from_slice(&b);
+
+    let pa = p - a;
+    let ba = b - a;
+
+    let h = (pa.dot(&ba) / ba.dot(&ba)).clamp(0., 1.);
+
+    distance(&Point2::from(h * ba), &Point2::from(pa))
 }
